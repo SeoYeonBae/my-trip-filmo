9ddeaefd1e2ce2d8a82cbc44d722844e9e351126
tjdus2033 <tjdus2033@naver.com>
2022-11-22 17:16:39 +0900

    design: 메인 커버 페이지 디자인 변경

--- Front-End/src/views/AppMain.vue
+++ Front-End/src/views/AppMain.vue
@@ -1,28 +1,31 @@
 <template>
   <div>
-    <main-cover-slider></main-cover-slider>
+    <main-cover></main-cover>
+    <!-- <main-cover-slider></main-cover-slider>
     <main-tour-recommend></main-tour-recommend>
-    <main-contents-panel></main-contents-panel>
-    <main-weather></main-weather>
-    <main-weather2></main-weather2>
+    <main-contents-panel></main-contents-panel> -->
+    <!-- <main-weather></main-weather>
+    <main-weather2></main-weather2> -->
   </div>
 </template>
 
 <script>
-import MainCoverSlider from "@/components/main/MainCoverSlider";
-import MainTourRecommend from "@/components/main/MainTourRecommend";
-import MainContentsPanel from "@/components/main/MainContentsPanel";
-import MainWeather from "@/components/main/MainWeather";
-import MainWeather2 from "@/components/main/MainWeather2";
+import MainCover from "@/components/main/MainCover";
+// import MainCoverSlider from "@/components/main/MainCoverSlider";
+// import MainTourRecommend from "@/components/main/MainTourRecommend";
+// import MainContentsPanel from "@/components/main/MainContentsPanel";
+// import MainWeather from "@/components/main/MainWeather";
+// import MainWeather2 from "@/components/main/MainWeather2";
 
 export default {
   name: "AppMain",
   components: {
-    MainCoverSlider,
-    MainTourRecommend,
-    MainContentsPanel,
-    MainWeather,
-    MainWeather2,
+    MainCover,
+    // MainCoverSlider,
+    // MainTourRecommend,
+    // MainContentsPanel,
+    // MainWeather,
+    // MainWeather2,
   },
 };
 </script>
